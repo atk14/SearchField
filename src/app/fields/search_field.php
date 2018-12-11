8e4258941ef876a309f1216611a060b7ae53a478
Jaromir Tomek <jaromir.tomek@gmail.com>
2018-12-11 13:51:44 +0100

    SearchField and SearchInput added :)

---
+++ src/app/fields/search_field.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Field for a search input :)
+ */
+class SearchField extends CharField{
+
+	function __construct($options = array()){
+		$options += array(
+			"widget" => new SearchInput()
+		);
+		parent::__construct($options);
+	}
+}
