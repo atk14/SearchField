2972fd37918fa51ab4d1de0f667c63c1960e143e
Jaromir Tomek <jaromir.tomek@gmail.com>
2018-12-11 14:12:14 +0100

    By default the field is not required and max_length is set to 200

--- src/app/fields/search_field.php
+++ src/app/fields/search_field.php
@@ -1,12 +1,16 @@
 <?php
 /**
- * Field for a search input :)
+ * Field for a search input
+ *
+ * By default the field is not required and max_length is set to 200.
  */
 class SearchField extends CharField{
 
 	function __construct($options = array()){
 		$options += array(
-			"widget" => new SearchInput()
+			"widget" => new SearchInput(),
+			"required" => false,
+			"max_length" => 200,
 		);
 		parent::__construct($options);
 	}
